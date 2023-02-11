d343f3f59bae64d3ce073b805fda6a1920bf7091
Rakshit <rakshitsharma1605@gmail.com>
2023-02-12 01:40:30 +0530

    finished

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,18 +1,80 @@
 import { IconBtn } from './IconBtn';
-import { FaEdit, FaHeart, FaReply, FaTrash } from 'react-icons/fa';
+import { FaEdit, FaHeart, FaReply, FaTrash, FaRegHeart } from 'react-icons/fa';
 import { usePost } from '../contexts/PostContext';
 import { useState } from 'react';
 import { CommentList } from './CommentList';
+import { CommentForm } from './CommentForm';
+import { useAsyncFn } from '../hooks/useAsync';
+import {
+  createComment,
+  updateComment,
+  deleteComment,
+  toggleCommentLike,
+} from '../services/comments';
+import { useUser } from '../hooks/useUser';
 
 const dateFormatter = new Intl.DateTimeFormat(undefined, {
   dateStyle: 'medium',
   timeStyle: 'short',
 });
 
-export function Comment({ id, message, user, createdAt }) {
+export function Comment({
+  id,
+  message,
+  user,
+  createdAt,
+  likeCount,
+  likedByMe,
+}) {
   const [areChildrenHidden, setAreChildrenHidden] = useState(false);
-  const { getReplies } = usePost();
+  const [isReplying, setIsReplying] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const {
+    post,
+    getReplies,
+    createLocalComment,
+    updateLocalComment,
+    deleteLocalComment,
+    toggleLocalCommentLike,
+  } = usePost();
+  const createCommentFn = useAsyncFn(createComment);
+  const updateCommentFn = useAsyncFn(updateComment);
+  const deleteCommentFn = useAsyncFn(deleteComment);
+  const toggleCommentLikeFn = useAsyncFn(toggleCommentLike);
   const childComments = getReplies(id);
+  const currentUser = useUser();
+
+  function onCommentReply(message) {
+    return createCommentFn
+      .execute({ postId: post.id, message, parentId: id })
+      .then(comment => {
+        setIsReplying(false);
+        createLocalComment(comment);
+      });
+  }
+
+  function onCommentUpdate(message) {
+    return updateCommentFn
+      .execute({ postId: post.id, message, id })
+      .then(comment => {
+        setIsEditing(false);
+
+        updateLocalComment(id, comment.message);
+      });
+  }
+
+  function onCommentDelete() {
+    return deleteCommentFn
+      .execute({ postId: post.id, id })
+      .then(comment => deleteLocalComment(comment.id));
+  }
+
+  function onToggleCommentLike() {
+    return toggleCommentLikeFn
+      .execute({ id, postId: post.id })
+      .then(({ addLike }) => toggleLocalCommentLike(id, addLike));
+  }
+
   return (
     <>
       <div className="comment">
@@ -22,17 +84,69 @@ export function Comment({ id, message, user, createdAt }) {
             {dateFormatter.format(Date.parse(createdAt))}
           </span>
         </div>
-        <div className="message">{message}</div>
-        <div className="footer">
-          <IconBtn Icon={FaHeart} aria-label="Like">
-            2
-          </IconBtn>
-          <IconBtn Icon={FaReply} aria-label="Reply" />
 
-          <IconBtn Icon={FaEdit} aria-label="Edit" />
-          <IconBtn Icon={FaTrash} aria-label="Delete" color="danger" />
+        {isEditing ? (
+          <CommentForm
+            autoFocus
+            initialValue={message}
+            onSubmit={onCommentUpdate}
+            loading={updateCommentFn.loading}
+            error={updateCommentFn.error}
+          />
+        ) : (
+          <div className="message">{message}</div>
+        )}
+
+        <div className="footer">
+          <IconBtn
+            onClick={onToggleCommentLike}
+            disabled={toggleCommentLikeFn.loading}
+            Icon={likedByMe ? FaHeart : FaRegHeart}
+            aria-label={likedByMe ? 'Unlike' : 'Like'}
+          >
+            {likeCount}
+          </IconBtn>
+          <IconBtn
+            onClick={() => setIsReplying(prev => !prev)}
+            isActive={isReplying}
+            Icon={FaReply}
+            aria-label={isReplying ? 'Cancel Reply' : 'Reply'}
+          />
+
+          {user.id === currentUser.id && (
+            <>
+              <IconBtn
+                onClick={() => setIsEditing(prev => !prev)}
+                isActive={isEditing}
+                Icon={FaEdit}
+                aria-label={isEditing ? 'Cancel Edit' : 'Edit'}
+              />
+              <IconBtn
+                disabled={deleteCommentFn.loading}
+                onClick={onCommentDelete}
+                Icon={FaTrash}
+                aria-label="Delete"
+                color="danger"
+              />
+            </>
+          )}
         </div>
+        {deleteCommentFn.error && (
+          <div className="error-msg mt-1">{deleteCommentFn.error}</div>
+        )}
       </div>
+
+      {isReplying && (
+        <div className="mt-1 ml-3">
+          <CommentForm
+            autoFocus
+            onSubmit={onCommentReply}
+            loading={createCommentFn.loading}
+            error={createCommentFn.error}
+          />
+        </div>
+      )}
+
       {childComments?.length > 0 && (
         <>
           <div
--- client/src/contexts/PostContext.js
+++ client/src/contexts/PostContext.js
@@ -1,4 +1,4 @@
-import React, { useContext, useMemo } from 'react';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAsync } from '../hooks/useAsync';
 import { getPost } from '../services/posts';
@@ -13,21 +13,74 @@ export function PostProvider({ children }) {
   const { id } = useParams();
   const { loading, error, value: post } = useAsync(() => getPost(id), [id]);
 
-  const commentsByParentId = useMemo(() => {
-    if (post?.comments == null) return [];
+  const [comments, setComments] = useState([]);
 
+  const commentsByParentId = useMemo(() => {
     const group = {};
 
-    post.comments.forEach(comment => {
+    comments.forEach(comment => {
       group[comment.parentId] ||= [];
       group[comment.parentId].push(comment);
     });
 
     return group;
+  }, [comments]);
+
+  useEffect(() => {
+    if (post?.comments == null) return;
+    setComments(post.comments);
   }, [post?.comments]);
 
   function getReplies(parentId) {
     return commentsByParentId[parentId];
+  }
+
+  function createLocalComment(comment) {
+    setComments(prevComments => {
+      return [comment, ...prevComments];
+    });
+  }
+
+  function updateLocalComment(id, message) {
+    setComments(prevComments => {
+      return prevComments.map(comment => {
+        if (comment.id === id) {
+          return { ...comment, message };
+        } else {
+          return comment;
+        }
+      });
+    });
+  }
+
+  function deleteLocalComment(id) {
+    setComments(prevComments => {
+      return prevComments.filter(comment => comment.id !== id);
+    });
+  }
+
+  function toggleLocalCommentLike(id, addLike) {
+    setComments(prevComments => {
+      return prevComments.map(comment => {
+        if (id === comment.id) {
+          if (addLike) {
+            return {
+              ...comment,
+              likeCount: comment.likeCount + 1,
+              likedByMe: true,
+            };
+          } else {
+            return {
+              ...comment,
+              likeCount: comment.likeCount - 1,
+              likedByMe: false,
+            };
+          }
+        } else {
+          return comment;
+        }
+      });
+    });
   }
 
   return (
@@ -36,6 +89,10 @@ export function PostProvider({ children }) {
         post: { id, ...post },
         rootComments: commentsByParentId[null],
         getReplies,
+        createLocalComment,
+        updateLocalComment,
+        deleteLocalComment,
+        toggleLocalCommentLike,
       }}
     >
       {loading ? (
